637bf1fa2a95f77904fdbfa1d5fc953c3acdf91b
duongpt582 <tduong.hust02@gmail.com>
2025-03-20 09:56:14 +0700

    Management Dependency by Container

---
+++ app/src/main/java/com/duongpt/manualdi/AppContainer.kt
@@ -0,0 +1,25 @@
+package com.duongpt.manualdi
+
+import com.duongpt.manualdi.login.LoginRetrofitService
+import com.duongpt.manualdi.login.LoginService
+import com.duongpt.manualdi.login.LoginViewModelFactory
+import com.duongpt.manualdi.login.UserLocalDataSource
+import com.duongpt.manualdi.login.UserRemoteDataSource
+import com.duongpt.manualdi.login.UserRepository
+import retrofit2.Retrofit
+
+class AppContainer {
+    private val loginService = Retrofit.Builder()
+        .baseUrl("https://example.com")
+        .build()
+        .create(LoginService::class.java)
+
+    // Then, satisfy the dependencies of UserRepository
+    val remoteDataSource = UserRemoteDataSource(LoginRetrofitService(loginService))
+    val localDataSource = UserLocalDataSource()
+
+    // Now you can create an instance of UserRepository that LoginViewModel needs
+    val userRepository = UserRepository(localDataSource, remoteDataSource)
+
+    val loginViewModelFactory = LoginViewModelFactory(userRepository)
+}
